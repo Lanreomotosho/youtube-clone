cb1dc0cb314286a464c4c25322c9abaf36e0989e
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-01-24 23:28:36 +0100

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as feed } from './feed';
 export { default as channeldetail } from './channeldetail';
 export { default as searchfeed } from './searchfeed';
 export { default as videodetail } from './videodetail';
+export { default as searchbar } from './searchbar';
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const navbar = () => {
-  return (
-    <div>navbar</div>
+import { logo } from '../utils/constants';
+import searchbar from "./searchbar";
+
+const navbar = () => (
+ <Stack 
+ direction="row" 
+ alighItems="center" 
+ p={2} 
+ sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
+ >
+
+<Link to ="/" style={{ display: 'flex', alignItems: 'center'}}>
+<img src={logo} alt="logo" height={45} />
+</Link>
+<searchbar />
+ </Stack>
   )
-}
+
 
 export default navbar
