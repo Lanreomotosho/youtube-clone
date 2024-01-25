11bb352ec0b731b107c3aff4473737ae2a1a026c
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-01-25 17:29:36 +0100

    first commit

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
 
 const Feed = () => {
   return (
-    <div>feed</div>
+ <Stack sx={{ flexDirection: { sx:
+"column", md: "row" }}}>
+  <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}  }}>
+Sidebar
+
+<Typography className='copyright' varient='body2' sx={{ mt: 1.5, color: '#fff'}}>
+Copyright 2024 Lanre Omotosho
+
+</Typography>
+  </Box>
+ </Stack>
   )
 }
 
